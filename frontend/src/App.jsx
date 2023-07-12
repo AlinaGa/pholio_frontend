df0ccdeca2c25349d260108ce25f64ebc4a26d34
Alina Gattermann <gattermannalina@gmail.com>
2023-07-12 17:07:25 +0200

    general css setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';  
+
 
 function App() {
 
