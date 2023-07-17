4174c90e594e9c17dc4a3e008ff431ac2b3319a2
Alina Gattermann <gattermannalina@gmail.com>
2023-07-17 09:22:48 +0200

    changed location of assets folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import AdminNavbar from './components/photographer/AdminNavbar';
 import SideBar from './components/photographer/Sidebar';
 import GalleryModal from './components/photographer/GalleryModal';
 import LoginPage from './components/photographer/LoginPage';
-import AdminMain from './components/photographer/AdminMain'; 
+import AdminMain from './components/photographer/AdminMain';
 import LoginNavbar from './components/photographer/LoginNavbar';
 import LoginMain from './components/photographer/LoginMain';
 import ClientModal from './components/photographer/ClientModal';
@@ -19,40 +19,40 @@ import Shop from './pages/clientpages/Shop';
 import './App.css';
 import "../src/components/landingpage/Landingpage.css";
 import './components/client/client.css';
->>>>> main2
+
 
 function App() {
   return (
 
 
     <>
-      <LandingPage />
-// <AlbumList />
-    <Shop />
+      {/* <LandingPage /> */}
+      <AlbumList /> 
+      {/* <Shop /> */}
 
 
-{/* <LoginMain />  */}
-{/* 
+      {/* <LoginMain />  */}
+      {/* 
 < LoginNavbar />
 <LoginPage /> */}
 
 
-{/* 
+      {/* 
 <ClientModal /> */}
-{/* < AdminMain /> */}
-{/* <AdminNavbar />  */}
-{/* <SideBar /> */}
+      {/* < AdminMain /> */}
+      {/* <AdminNavbar />  */}
+      {/* <SideBar /> */}
 
 
 
-{/* < GalleryModal /> */}
+      {/* < GalleryModal /> */}
 
-//   <Routes>
-    //     <Route path="/" element={<Login />} />
-    //     {/* <Route path="/:photographerId/*" element={<ClientLogin />} /> */}
-    //     {/* <Route path="/:photographerId/:clientId" element={<AlbumList />} /> */}
-    //     {/* <Route path="/:photographerId/:clientId/:galleryId" element={<SpecificGallery />} /> */}
-    //   </Routes>
+      {/* <Routes> */}
+        {/* <Route path="/" element={<Login />} /> */}
+        {/* <Route path="/:photographerId/*" element={<ClientLogin />} /> */}
+        {/* <Route path="/:photographerId/:clientId" element={<AlbumList />} /> */}
+        {/* <Route path="/:photographerId/:clientId/:galleryId" element={<SpecificGallery />} /> */}
+      {/* </Routes> */}
 
 
     </>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <BrowserRouter> */}
     <App />
-  /* </BrowserRouter> */}
+    {/* </BrowserRouter> */}
 
   </React.StrictMode>
 );
